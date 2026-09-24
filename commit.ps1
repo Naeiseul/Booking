54b33ef4cfe3e6a88e550e672d2212d5583e2ef7
Naeiseul <naeiseul@proton.me>
2026-09-25 01:05:40 +0200

    Delete commit.ps1

--- commit.ps1
+++
@@ -1,9 +0,0 @@
-$gitPath = (Get-ChildItem -Path "$env:LOCALAPPDATA\GitHubDesktop\app-*\resources\app\git\cmd\git.exe" -ErrorAction SilentlyContinue | Select-Object -First 1).FullName
-if (-not $gitPath) { $gitPath = "C:\Program Files\Git\cmd\git.exe" }
-if (Test-Path $gitPath) {
-    & $gitPath add .
-    & $gitPath commit -m "Fix: Updated toast copywriting for better grammar"
-    Write-Host "Success"
-} else {
-    Write-Host "Could not find git"
-}
