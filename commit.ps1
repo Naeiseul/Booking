cfcbbd4fd74cef9b821a037af387f086b752e8b0
Naeiseul <naeiseul@proton.me>
2026-09-25 00:56:38 +0200

    Fix: Anchored container to top and set min-height to prevent jumping

--- commit.ps1
+++ commit.ps1
@@ -2,7 +2,7 @@ $gitPath = (Get-ChildItem -Path "$env:LOCALAPPDATA\GitHubDesktop\app-*\resources
 if (-not $gitPath) { $gitPath = "C:\Program Files\Git\cmd\git.exe" }
 if (Test-Path $gitPath) {
     & $gitPath add .
-    & $gitPath commit -m "Feat: Made admin hours dynamic via URL parameters"
+    & $gitPath commit -m "Fix: Anchored container to top and set min-height to prevent jumping"
     Write-Host "Success"
 } else {
     Write-Host "Could not find git"
