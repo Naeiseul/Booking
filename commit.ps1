eb4cbac6d4ac7e224b03395d992e64ff042a4a48
Naeiseul <naeiseul@proton.me>
2026-09-25 01:02:18 +0200

    Delete commit.ps1

--- commit.ps1
+++
@@ -1,9 +0,0 @@
-$gitPath = (Get-ChildItem -Path "$env:LOCALAPPDATA\GitHubDesktop\app-*\resources\app\git\cmd\git.exe" -ErrorAction SilentlyContinue | Select-Object -First 1).FullName
-if (-not $gitPath) { $gitPath = "C:\Program Files\Git\cmd\git.exe" }
-if (Test-Path $gitPath) {
-    & $gitPath add .
-    & $gitPath commit -m "Fix: Switched to 100dvh and disabled overscroll to stop mobile page bouncing"
-    Write-Host "Success"
-} else {
-    Write-Host "Could not find git"
-}
