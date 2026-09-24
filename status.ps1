e3baf978750ad0f5f368da6be07c77dfbc8c7e12
Naeiseul <naeiseul@proton.me>
2026-09-25 01:46:48 +0200

    Create status.ps1

---
+++ status.ps1
@@ -0,0 +1,8 @@
+$gitPath = (Get-ChildItem -Path "$env:LOCALAPPDATA\GitHubDesktop\app-*\resources\app\git\cmd\git.exe" -ErrorAction SilentlyContinue | Select-Object -First 1).FullName
+if (-not $gitPath) { $gitPath = "C:\Program Files\Git\cmd\git.exe" }
+if (Test-Path $gitPath) {
+    & $gitPath status
+    Write-Host "Success"
+} else {
+    Write-Host "Could not find git"
+}
