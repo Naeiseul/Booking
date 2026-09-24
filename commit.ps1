a4261894ec3b86685bc3fc137f76b332c8b0230d
Naeiseul <naeiseul@proton.me>
2026-09-25 01:12:23 +0200

    Delete commit.ps1

--- commit.ps1
+++
@@ -1,9 +0,0 @@
-$gitPath = (Get-ChildItem -Path "$env:LOCALAPPDATA\GitHubDesktop\app-*\resources\app\git\cmd\git.exe" -ErrorAction SilentlyContinue | Select-Object -First 1).FullName
-if (-not $gitPath) { $gitPath = "C:\Program Files\Git\cmd\git.exe" }
-if (Test-Path $gitPath) {
-    & $gitPath add .
-    & $gitPath commit -m "Refactor: Swapped dynamic URL hours for a static 7AM-8PM grid for ultimate owner freedom"
-    Write-Host "Success"
-} else {
-    Write-Host "Could not find git"
-}
