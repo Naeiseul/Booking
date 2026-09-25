79ab0448d29c359176ace641890651e57d27301c
Naeiseul <naeiseul@proton.me>
2026-09-25 02:08:30 +0200

    Delete commit.ps1

--- commit.ps1
+++
@@ -1,9 +0,0 @@
-$gitPath = (Get-ChildItem -Path "$env:LOCALAPPDATA\GitHubDesktop\app-*\resources\app\git\cmd\git.exe" -ErrorAction SilentlyContinue | Select-Object -First 1).FullName
-if (-not $gitPath) { $gitPath = "C:\Program Files\Git\cmd\git.exe" }
-if (Test-Path $gitPath) {
-    & $gitPath add .
-    & $gitPath commit -m "Feat: Enabled clean URLs via Vercel rewrites and dynamic JS routing"
-    Write-Host "Success"
-} else {
-    Write-Host "Could not find git"
-}
